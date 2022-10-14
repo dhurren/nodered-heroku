3dda639bd3b6be98b6d75960e31f4e3ac36db937
dhurren <dc.hurren@gmail.com>
2022-10-14 16:24:41 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -15,14 +15,14 @@ var { createProxyMiddleware } = require('http-proxy-middleware'); 
 var proxy = require('express-http-proxy');   
 var app = require('express')();  
 
-var port1 = (parseInt(process.env.PORT) + 2) 
+var port1 = (parseInt(process.env.PORT) + 1) 
 console.log( "port=" + port1 ) 
 
 //const wsProxy = createProxyMiddleware( {
 //    target: 'ws://localhost/editor/comms', ws: true, changeOrigin: true
 //});
 
-const wsProxy = createProxyMiddleware({ target: 'http://localhost:'+port1+'/', ws: true });
+//const wsProxy = createProxyMiddleware({ target: 'http://localhost:'+port1+'/', ws: true });
 
 //app.use( '/', wsProxy );
 app.use( '/', proxy('http://localhost:'+port1+'/')); 

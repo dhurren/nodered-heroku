1d6c9934e68f75280d56e135fbfe1a1f377e7807
dhurren <dc.hurren@gmail.com>
2022-10-14 04:02:53 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -11,11 +11,20 @@ app.listen(process.env.PORT, function () { console.log("Proxy listening") } )
 */
 
 
+var httpproxy = require('http-proxy-middleware');  
 var proxy = require('express-http-proxy');   
 var app = require('express')();  
 
 var port1 = (parseInt(process.env.PORT) + 1) 
 console.log( "port=" + port1 ) 
 
+const wsProxy = httpproxy('/', {
+    target: 'ws://localhost',
+    ws: true
+});
+
+app.use( wsProxy );
 app.use('/', proxy('http://localhost:'+port1+'/')); 
 app.listen(process.env.PORT, function () { console.log("Proxy listening") } )
+
+

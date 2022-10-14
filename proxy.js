951829e3b10c471063fe72616fbc3aee91ef68b0
dhurren <dc.hurren@gmail.com>
2022-10-14 03:33:34 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -11,15 +11,11 @@ app.listen(process.env.PORT, function () { console.log("Proxy listening") } )
 */
 
 
-var http = require('http'),
-    httpProxy = require('http-proxy');
- 
+var proxy = require('express-http-proxy');   
+var app = require('express')();  
 
-var proxy = httpProxy.createProxyServer({});
- 
-var server = http.createServer(function(req, res) {
-  proxy.web(req, res, { target: 'https://enjine.cloud' });
-});
- 
-console.log("listening on port: " + process.env.PORT)
-server.listen( process.env.PORT );
+var port1 = (parseInt(process.env.PORT) + 1) 
+console.log( "port=" + port1 ) 
+
+app.use('/', proxy('https://enjine.cloud/')); 
+app.listen(process.env.PORT, function () { console.log("Proxy listening") } )

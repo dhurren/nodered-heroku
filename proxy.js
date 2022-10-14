706bb3c8d9dbea21c8a78e8f56baf5379151142e
dhurren <dc.hurren@gmail.com>
2022-10-14 01:57:19 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,3 +1,5 @@
+
+/* ok but websockets don't work
 var proxy = require('express-http-proxy');   
 var app = require('express')();  
 
@@ -6,3 +8,31 @@ console.log( "port=" + port1 )
 
 app.use('/', proxy('http://localhost:'+port1+'/')); 
 app.listen(process.env.PORT, function () { console.log("Proxy listening") } )
+*/
+
+var http = require('http'),
+    httpProxy = require('http-proxy'),
+    express = require('express');
+
+var port1 = (parseInt(process.env.PORT) + 1) 
+console.log( "port=" + port1 ) 
+
+var app = express();
+var proxy = httpProxy.createProxyServer({ target: 'http://localhost:'+port1+'/', ws: true });
+var server = require('http').createServer(app);
+
+// proxy HTTP GET / POST
+app.get('/*', function(req, res) {
+  proxy.web(req, res, {});
+});
+app.post('/*', function(req, res) {
+  proxy.web(req, res, {});
+});
+
+// Proxy websockets
+server.on('upgrade', function (req, socket, head) {
+  proxy.ws(req, socket, head);
+});
+
+
+server.listen( process.env.PORT );

eb469f4d7ddc031db5e1259bd548963d22da75ea
dhurren <dc.hurren@gmail.com>
2022-10-17 14:06:22 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -15,18 +15,23 @@ var { createProxyMiddleware } = require('http-proxy-middleware'); 
 var proxy = require('express-http-proxy');   
 var app = require('express')();  
 
-var port1 = (parseInt(process.env.PORT) + 1) 
-console.log( "port=" + port1 ) 
+var redport = (parseInt(process.env.PORT) + 1) 
+console.log( "node red port=" + redport ) 
 
 //const wsProxy = createProxyMiddleware( {
 //    target: 'ws://localhost/editor/comms', ws: true, changeOrigin: true
 //});
 
-const wsProxy = createProxyMiddleware({ target: 'http://localhost:'+port1+'/', ws: true, 
-    router: { '/editor0': 'http://localhost:'+port1, '/editor1': 'http://localhost:'+(port1+1), } });
+const wsProxy = createProxyMiddleware({ target: 'http://localhost:'+redport+'/', ws: true, 
+    router: { '/editor1': 'http://localhost:'+redport, 
+              '/editor2': 'http://localhost:'+(redport+1), 
+              '/editor3': 'http://localhost:'+(redport+2),
+              '/editor4': 'http://localhost:'+(redport+3),
+              '/editor': 'http://localhost:'+redport
+            } });
 
 app.use( '/', wsProxy );
-//app.use( '/', proxy('http://localhost:'+port1+'/')); 
+//app.use( '/', proxy('http://localhost:'+redport+'/')); 
 app.listen( process.env.PORT, function () { console.log("Proxy listening") } )
 
 

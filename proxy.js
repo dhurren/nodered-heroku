01e708076ac4c5a6babc568977a1cc99a55355ed
dhurren <dc.hurren@gmail.com>
2022-10-24 18:36:15 +0100

    Added round-robin load balancing

--- proxy.js
+++ proxy.js
@@ -1,45 +1,38 @@
 
-/* ok but websockets don't work
-var proxy = require('express-http-proxy');   
-var app = require('express')();  
+var { createProxyMiddleware } = require('http-proxy-middleware');  
+var proxy = require('express-http-proxy');   
+var app = require('express')();  
 
-var port1 = (parseInt(process.env.PORT) + 1) 
-console.log( "port=" + port1 ) 
-
-app.use('/', proxy('http://localhost:'+port1+'/')); 
-app.listen(process.env.PORT, function () { console.log("Proxy listening") } )
-*/
-
-
-var { createProxyMiddleware } = require('http-proxy-middleware');  
-var proxy = require('express-http-proxy');   
-var app = require('express')();  
-
-var redport = (parseInt(process.env.PORT) + 1) 
+var redport = (parseInt(process.env.PORT) + 1) 
 console.log( "node red port=" + redport ) 
-
-//const wsProxy = createProxyMiddleware( {
-//    target: 'ws://localhost/editor/comms', ws: true, changeOrigin: true
-//});
+var instance = 0
 
 const wsProxy = createProxyMiddleware({ 
     target: 'http://localhost:'+redport+'/', ws: true, changeOrigin: true, 
     pathRewrite: { 
-        '^/instance1':'', '^/instance2':'', '^/instance3':'', '^/instance4':''
-    },
-    router: { 
-        '/editor1': 'http://localhost:'+redport, 
-        '/editor2': 'http://localhost:'+(redport+1), 
-        '/editor3': 'http://localhost:'+(redport+2),
-        '/editor4': 'http://localhost:'+(redport+3),
-        '/instance1': 'http://localhost:'+redport, 
-        '/instance2': 'http://localhost:'+(redport+1), 
-        '/instance3': 'http://localhost:'+(redport+2),
-        '/instance4': 'http://localhost:'+(redport+3)
-     } });
+        '^/instance1':'', '^/instance2':'', '^/instance3':'', '^/instance4':'', 
+        '^/editor$':'/editor1', '^/editor/':'/editor1/'
+    }, 
+    router: function(req) {
+        
+        var i = 0
+        if( req.url.startsWith("/favicon.ico") ) i = 0
+        else if( req.url.startsWith("/editor1") ) i = 0
+        else if( req.url.startsWith("/editor2") ) i = 1
+        else if( req.url.startsWith("/editor3") ) i = 2
+        else if( req.url.startsWith("/editor") ) i = 0
+        else if( req.url.startsWith("/ui") ) i = 0
+        else if( req.url.startsWith("/instance1") ) i = 0
+        else if( req.url.startsWith("/instance2") ) i = 1
+        else if( req.url.startsWith("/instance3") ) i = 2
+        else { 
+            if( instance++==2 ) instance = 0; 
+            i = instance 
+        }
+
+        return 'http://localhost:'+(redport+i)
+    }
+  });
 
 app.use( '/', wsProxy );
-//app.use( '/', proxy('http://localhost:'+redport+'/')); 
-app.listen( process.env.PORT, function () { console.log("Proxy listening") } )
-
-
+app.listen( process.env.PORT, function () { console.log("Proxy listening") } )
